e12b9e7260e62b2e87e4426b6b8f09ad696c1641
DeepakYiron <deepak.chauhan@yirontechnologies.com>
2023-01-11 15:22:02 +0530

    test

---
+++ android/app/src/main/kotlin/com/example/goolgle_map_master/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.goolgle_map_master
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,176 @@
+import 'dart:async';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_polyline_points/flutter_polyline_points.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatefulWidget{
+  const Home({super.key});
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  final Completer<GoogleMapController> _controller = Completer();
+  GoogleMapController? mapController; //contrller for Google map
+  PolylinePoints polylinePoints = PolylinePoints();
+
+  String googleAPiKey = "AIzaSyDJki66xdJ8dZy72cs0XoGqhCA0LNTfsdo";
+
+  Set<Marker> markers = {}; //markers for google map
+  Map<PolylineId, Polyline> polylines = {}; //polylines to show direction
+
+  LatLng startLocation = const LatLng(27.6683619, 85.3101895);
+  LatLng endLocation = const LatLng(27.6688312, 85.3077329);
+
+  LatLng? currentLocation;
+
+  @override
+  void initState() {
+    markers.add(Marker( //add start location marker
+      markerId: MarkerId(startLocation.toString()),
+      position: startLocation, //position of marker
+      infoWindow: const InfoWindow( //popup info
+        title: 'Starting Point ',
+        snippet: 'Start Marker',
+      ),
+      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
+    ));
+
+    markers.add(Marker( //add distination location marker
+      markerId: MarkerId(endLocation.toString()),
+      position: endLocation, //position of marker
+      infoWindow: const InfoWindow( //popup info
+        title: 'Destination Point ',
+        snippet: 'Destination Marker',
+      ),
+      icon: BitmapDescriptor.defaultMarker, //Icon for Marker
+    ));
+
+    getDirections(); //fetch direction polylines from Google API
+
+    super.initState();
+  }
+
+  getDirections() async {
+    List<LatLng> polylineCoordinates = [];
+
+    PolylineResult result = await polylinePoints.getRouteBetweenCoordinates(googleAPiKey,
+      PointLatLng(startLocation.latitude, startLocation.longitude),
+      PointLatLng(endLocation.latitude, endLocation.longitude),
+      travelMode: TravelMode.driving,
+    );
+
+    if (result.points.isNotEmpty) {
+      for (var point in result.points) {
+        polylineCoordinates.add(LatLng(point.latitude, point.longitude));
+      }
+    } else {
+      if (kDebugMode) {
+        print(result.errorMessage);
+      }
+    }
+    addPolyLine(polylineCoordinates);
+  }
+
+  addPolyLine(List<LatLng> polylineCoordinates) {
+    PolylineId id = const PolylineId("poly");
+    Polyline polyline = Polyline(
+      polylineId: id,
+      color: Colors.deepPurpleAccent,
+      points: polylineCoordinates,
+      width: 8,
+    );
+    polylines[id] = polyline;
+    setState(() {});
+  }
+
+  Future<Position> getUserCurrentLocation() async {
+    await Geolocator.requestPermission().then((value){
+    }).onError((error, stackTrace) async {
+      await Geolocator.requestPermission();
+      if (kDebugMode) {
+  //
+      }
+    });
+    return await Geolocator.getCurrentPosition();
+  }
+
+
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async{
+          getUserCurrentLocation().then((value) async {
+            LatLng currentLoc=LatLng(value.latitude, value.longitude);
+            setState(() {
+              currentLocation=currentLoc;
+            });
+            if (kDebugMode) {
+              print("${value.latitude} ${value.longitude}");
+            }
+
+            // marker added for current users location
+            markers.add(
+                Marker(
+                  markerId: const MarkerId("2"),
+                  position: LatLng(value.latitude, value.longitude),
+                  infoWindow: const InfoWindow(
+                    title: 'My Current Location',
+                  ),
+                )
+            );
+
+            // specified current users location
+            CameraPosition cameraPosition = CameraPosition(
+              target: LatLng(value.latitude, value.longitude),
+              zoom: 14,
+            );
+
+            final GoogleMapController controller = await _controller.future;
+            controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+            setState(() {
+            });
+          });
+        },
+        child: Icon(Icons.local_activity),
+      ),
+      appBar: AppBar(
+        title: const Text("Route Driection in Google Map"),
+        backgroundColor: Colors.deepPurpleAccent,
+      ),
+      body: GoogleMap( //Map widget from google_maps_flutter package
+        zoomGesturesEnabled: true, //enable Zoom in, out on map
+        initialCameraPosition: CameraPosition(
+          target: currentLocation??const LatLng(0.26456452, 99923.22), //initial position
+          zoom: 16.0, //initial zoom level
+        ),
+        markers: markers, //markers to show on map
+        polylines: Set<Polyline>.of(polylines.values), //polylines
+        mapType: MapType.normal, //map type
+        onMapCreated: (controller) { //method called when map is created
+          setState(() {
+            mapController = controller;
+          });
+        },
+      ),
+    );
+  }
+}
